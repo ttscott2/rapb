975823bae8fb2406b55ca06c42423fcdd2baf6b5
ttscott2 <timothytscott3@gmail.com>
2017-01-12 15:07:24 -0600

    start angular search

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dashboard#index'
   resources :customers, only: [ :index ]
+  get "angular_test" => "angular_test#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
